300ce122fb64ebaaf9b0deaf9924a14f8601db3f
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2024-09-21 22:24:31 +0200

    Name updated

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from src.api.task.router import router as task_router
 
 app = FastAPI(
-    title="Task Management Application API",
+    title="Task management service API",
     description="API for managing tasks including create, update, delete, and retrieve.",
     version="1.0",
 )
